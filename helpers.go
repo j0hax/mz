085f283715b39d283fdb87c2e9551c732af165f9
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2023-03-07 18:29:16 +0100

    Implement new details view

--- helpers.go
+++ helpers.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"sort"
-	"strings"
-
 	"github.com/j0hax/go-openmensa"
 	"github.com/rivo/tview"
 )
 
 // errHandler displays the given error message in the .detail view.
 func errHandler(err error) {
-	detailView.SetText("[red]Error:[-] " + err.Error())
+	notesView.SetText("[red]Error:[-] " + err.Error())
 }
 
 // loadCanteens retrieves canteens and populates the passed list with them.
@@ -27,20 +23,4 @@ func loadCanteens(list *tview.List) {
 	for _, m := range mensas {
 		list.AddItem(m.Name, m.Address, 0, nil)
 	}
-}
-
-func priceDisplay(prices map[string]float64) string {
-	var result []string
-	for k, v := range prices {
-		if v == 0 {
-			continue
-		}
-		n := titler.String(k)
-		result = append(result, fmt.Sprintf("%s: %.2f€", n, v))
-	}
-
-	// Sort array to maintain consistency
-	sort.Strings(result)
-
-	return strings.Join(result, ", ")
 }
